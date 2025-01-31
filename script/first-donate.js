22f9cde42155b1ef1bcef4803baf1d64a91312b7
abuyusufrony <abuyusufrony@gmail.com>
2025-01-31 11:32:43 +0600

    input-sum-added

--- script/first-donate.js
+++ script/first-donate.js
@@ -1,23 +1,36 @@
 document.getElementById('fst-donate').addEventListener('click',
     function () {
 
+
         const firstInput = getInputByuser('first-input');
-        sum += firstInput;
+        const firstMainbal = document.getElementById('mainBalnce').innerText;
+
 
 
         const firstspan = document.getElementById('first-span');
-        // const firstSpanValue = firstspan.innerText;
-        // const firsSpanValueFloat = parseFloat(firstSpanValue);
-        // console.log(typeof firsSpanValueFloat);
+        const firstSpanValue = firstspan.innerText;
+        const firsSpanValueFloat = parseFloat(firstSpanValue);
 
-        const firstMainbal = document.getElementById('mainBalnce').innerText;
-        const updateBal = firstMainbal - firstInput;
+        console.log(typeof firsSpanValueFloat);
+        const firstsum = firstInput + firsSpanValueFloat;
+        console.log(firstsum);
+
+
+
+        const updateBal = firstMainbal - firstsum;
         document.getElementById('mainBalnce').innerText = updateBal;
-        firstspan.innerText = firstInput;
+        firstspan.innerText = firstsum;
         const p = document.createElement('p');
 
         p.innerText = `added:${firstInput} tk. Eastern Flash Flood Situation :${updateBal}`;
         document.getElementById('history-sec').appendChild(p);
+
+
+
+
+
+
+
 
 
 
